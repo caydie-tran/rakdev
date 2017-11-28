8a644d98260a62cf28a332dc91576fcaa0a35688
Tran <caydie.tran@CaydieTran.local>
2017-11-28 10:33:58 -0800

    addressed comments from pr

--- assets/js/search.js
+++ assets/js/search.js
@@ -8,7 +8,6 @@
       for (var i = 0; i < results.length; i++) {  // Iterate over the results
         var item = store[results[i].ref];
         appendString += '<li><a href="' + item.searchlink + '"><h3>' + item.title + '</h3></a>';
-        // appendString += '<p>' + item.content.substring(0, 150) + '...</p></li>';
       }
 
       searchResults.innerHTML = appendString;
