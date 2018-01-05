d53dceb83257233a5ddcd8aa5ce79cae2288773e
Melvin <melvinb9@gmail.com>
2018-01-05 14:21:47 -0800

    more format/order changes

--- assets/javascript/dropdown.js
+++ assets/javascript/dropdown.js
@@ -1,6 +1,6 @@
 /* When the user clicks on the button, toggle between hiding and showing the dropdown content */
 function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
+  document.getElementById("myDropdown").classList.toggle("show");
 }
 
 // Close the dropdown menu if the user clicks outside of it
